61d0be2decb3150d2863836d9ca2e87f672c9c04
myself567 <sheriabhinayreddy@gmail.com>
2025-07-31 16:37:31 +0530

    Add files via upload

---
+++ weather-forecast-cli-app.py
@@ -0,0 +1,22 @@
+import requests
+
+api_key = '30d4741c779ba94c470ca1f63045390a'
+
+user_input = input("Enter city: ")
+
+weather_data = requests.get(
+    f"https://api.openweathermap.org/data/2.5/weather?q={user_input}&units=imperial&APPID={api_key}")
+
+if weather_data.json()['cod'] == '404':
+    print("No City Found")
+else:
+    weather = weather_data.json()['weather'][0]['main']
+    temp = weather_data.json()['main']['temp']
+    humidity =  weather_data.json()['main']['humidity']
+    condition =  weather_data.json()["weather"][0]["description"].title()
+   
+    temp_celsius = round((temp - 32) * 5 / 9)
+    print(f"The weather in {user_input} is: {weather}")
+    print(f"The temperature in {user_input} is: {temp_celsius}ºC")
+    print(f"The humidity in {user_input} is: {humidity}%")
+    print(f"The condition in {user_input} is: {condition}")
